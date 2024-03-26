f964b4996b57c01981a0e3a6b7aa6645df493e8d
chadmuro <chadmuro@gmail.com>
2024-03-27 00:07:38 +0900

    fix: Update generate numbers function logic

--- utils/generateNumbers.ts
+++ utils/generateNumbers.ts
@@ -2,15 +2,34 @@ export function generateRandomNumbers(
   count: number,
   excludeNumbers: number[]
 ): number[] {
-  const randomNumbers: number[] = [];
-
-  while (randomNumbers.length < count) {
-    const randomNumber: number = Math.floor(Math.random() * 2273) + 1;
-
-    if (!excludeNumbers.includes(randomNumber)) {
-      randomNumbers.push(randomNumber);
-    }
+  if (excludeNumbers.length >= 40) {
+    return [];
   }
 
-  return randomNumbers;
+  const remainingNumbers: number[] = [];
+  const availableNumbers: number[] = [];
+
+  // Generate all available numbers within the range (1 to 2273)
+  for (let i = 1; i <= 40; i++) {
+    availableNumbers.push(i);
+  }
+
+  // Filter out numbers that are in the excludeNumbers array
+  const availableNumbersFiltered = availableNumbers.filter(
+    (num) => !excludeNumbers.includes(num)
+  );
+
+  // Pick count number of random numbers from the available numbers
+  while (
+    remainingNumbers.length < count &&
+    availableNumbersFiltered.length > 0
+  ) {
+    const randomIndex = Math.floor(
+      Math.random() * availableNumbersFiltered.length
+    );
+    const randomNumber = availableNumbersFiltered.splice(randomIndex, 1)[0];
+    remainingNumbers.push(randomNumber);
+  }
+
+  return remainingNumbers;
 }
